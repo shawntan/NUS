7b92b3228b40307c3a93144beea79de13c799afa
Benjamin Tan <ben@witsvale.com>
2011-09-03 18:41:10 +0800

    problem set 1 question 2 done

--- CS4212/ps102.pl
+++ CS4212/ps102.pl
@@ -11,29 +11,60 @@
 :- op(959,xfx,of).
 :- op(959,xfx,::).
 :- op(959,xfx,=).
+:- dynamic count/1.
 
+count(0).
 
-count_assign(S;Ss, N) :- 
-	count_assign(S;, N1), count_assign(Ss, N2), N is N1+N2.
+statement(S;Ss) :- statement(S;), rest_statements(Ss), !, count(X), writeln(X).
+statement({S}) :- writeln('{'), statement(S), writeln('}').
 
-count_assign({S};, N) :- count_assign(S, N).
+statement(while X do Y;) :- 
+	write('while '), write('('), statement(X), write(') '), statement(Y), !.
 	
-count_assign({S}, N) :- count_assign(S, N).
-count_assign(S, N) :- 
-	(assignment_statement(S) -> N1 is N+1, writeln(N1));
-	(while_statement(S, N)).
-
-while_statement(while _ do Y;, N) :- count_assign(Y, N).
+statement(if X then Y;) :- 
+	write('if '), write('('), statement(X), write(') '), statement(Y), !.	
 	
-/*if_then_statement(if X then Y).
+statement(if X then Y else Z;) :- 
+	write('if '), write('('), statement(X), write(') '), statement(Y), write('else '), statement(Z), !.
 
-	
-if_then_else_statement(if X then Y else Z;, N).
-switch_statement(X :: Y;, N).
-switch_statement(X :: Y, N).*/
+statement(switch X of Y;) :- write('switch '), statement(X), statement(Y).
 
-assignment_statement(X=Y;) :- (identifier(X), value(Y)) ; (identifier(X), identifier(Y)).
+statement(X :: Y;) :-
+	write('case '), write(X), write(' : ' ), statement(Y), writeln('break ;'), !.
+
+statement(X :: Y) :-
+	write('case '), write(X), write(' : ' ), statement(Y), writeln('break ;'), !.
+
+statement(X\=Y)  :- write(X), write('!='), write(Y).
+statement(X\=Y;) :- write(X), write('!='), writeln(Y;).
+
+statement(S;) :- assignment_statement(S;); writeln(S;).
+statement(S)  :- assignment_statement(S); write(S). 
+
+rest_statements(S;Ss) :- statement(S), rest_statements(Ss).
+rest_statements(S)    :- statement(S).
+rest_statements       :- true.
+
+
+assignment_statement(X=Y;) :- 
+	((identifier(X), value(Y)) ; (identifier(X), identifier(Y))),
+	count(N), M is N+1, retractall(count(_)), assert(count(M)), !.
 
 identifier(X) :- atom(X),!.
 value(X) :- number(X),!.
 
+count_assignment_statement(X) :- retractall(count(_)), assert(count(0)), statement(X).
+
+
+
+/*
+
+count_assignment_statement(
+while (x>0) do {
+  switch (y+z) of {
+0 :: { if (x<10) then { a = 1 ; } else { a = 2; }; }; 1 ::{ if (y\=1) then {a=2; b=3;};}; default :: { a = 0 ; } ;
+};
+a=a+1; };
+).
+
+*/
