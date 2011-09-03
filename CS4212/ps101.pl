4ba7867b87639d39cda7aaa235846d3e8705cb6d
Benjamin Tan <ben@witsvale.com>
2011-09-03 10:29:26 +0800

    support for basic if statements

--- CS4212/ps101.pl
+++ CS4212/ps101.pl
@@ -1,12 +1,25 @@
 :- op(1099,yf,;).
 :- op(960,fx,while).
 :- op(959,xfx,do).
+:- op(960,fx,if).
+:- op(959,xfx,then).
+:- op(961,xfx,else).
 :- op(960,fx,for).
 
 
 statement(S;Ss) :- statement(S;), rest_statements(Ss),!.
 statement({S}) :- writeln('{'), statement(S), writeln('}').
-statement(while X do Y;) :- write('while '), write('('), write(X) ,write(') '), statement(Y), !.
+
+statement(while X do Y;) :- 
+	write('while '), write('('), write(X), write(') '), statement(Y), !.
+	
+statement(if X then Y;) :- 
+	write('if '), write('('), write(X), write(') '), statement(Y), !.	
+	
+statement(if X then Y else Z;) :- 
+	write('if '), write('('), write(X), write(') '), statement(Y), write('else '), statement(Z), !.
+
+
 statement(S;) :- writeln(S;).
 
 
@@ -31,4 +44,20 @@ while (x>0) {
             break ;
    default: { a = 0 ; }
 break ; }
-a = a+1 ; }*/
+a = a+1 ; }
+
+Test Cases for if:
+statement( if (z<0) then { x=a; } else { b=a; }; ).
+
+
+Test Cases for while:
+
+statement( while (a<0) do { b=a; }; ).
+statement( while (a<0) do { b=a; a=c; b=d; }; ).
+statement( while (a<0) do { while (a<0) do { b=a; }; a=c; b=d; }; ).
+statement( while (a<0) do { while (a<0) do { while (a<0) do { b=a; }; }; while (a<0) do { b=a; }; while (a<0) do { b=a; }; }; ).
+statement( a=b; while (a<0) do { while (a<0) do { while (a<0) do { b=a; }; }; while (a<0) do { b=a; }; while (a<0) do { b=a; }; }; ).
+statement( a=b; while (a<0) do { while (a<0) do { b=a; }; a=c; b=d; }; a=b; b=c; while (a<0) do { b=a; }; b=c;).
+
+
+*/
