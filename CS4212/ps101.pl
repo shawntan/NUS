95f340852f21508a0bca09345480a15a06e8c8ea
Benjamin Tan <ben@witsvale.com>
2011-09-07 12:37:59 +0800

    updated ps101 with comments

--- CS4212/ps101.pl
+++ CS4212/ps101.pl
@@ -1,4 +1,13 @@
-/* Benjamin Tan U077129N */
+% Benjamin Tan Wei Hao
+% U077129N
+
+/*
+	Problem 1 [2 marks]
+	
+	Write a translator from the toy language defined in Lecture 1, that outputs correct C code
+	fragments for the given code.
+
+*/
 
 :- op(1099,yf,;).
 :- op(960,fx,while).
@@ -12,21 +21,22 @@
 :- op(959,xfx,::).
 :- op(959,xfx,=).
 
-
+% Statements are composed of a single statement (terminated by a semi colon), and
+% other statements.
 statement(S;Ss) :- statement(S), rest_statements(Ss),!.
 
-% remove redundant braces.
+% remove redundant braces, by `absorbing` it in the head of the rule.
 statement({S;}) :- nl, statement(S;), !.
-
-
 statement({S;Ss}) :- writeln(' {' ), statement(S;Ss), writeln('}' ).
 
+% while statements
 statement(while X do Y;) :- 
 	write('while '), isExpr(X), statement(Y), !.
 
 statement(while X do Y) :- 
 	write('while '), isExpr(X), statement(Y), !.
 	
+% if statements 	
 statement(if X then Y;) :- 
 	write('if '), isExpr(X), statement(Y), !.	
 
@@ -39,10 +49,12 @@ statement(if X then Y else Z;) :-
 statement(if X then Y else Z) :- 
 	write('if '), isExpr(X), statement(Y), write('else '), statement(Z), !.
 
+% switch statements
 statement(switch X of Y;) :- write('switch '), isExpr(X),  statement(Y).
 statement(switch X of Y) :- write('switch '), isExpr(X),  statement(Y).
 
-
+% case statements. Constructing the predicates this way 'absorbs' the curly braces, 
+% effectively removing it.
 statement(X :: {Y};) :-
 	write('case '), write(X), writeln(' :' ), statement(Y), writeln('break ;'), !.
 
@@ -54,12 +66,13 @@ statement(S;) :- writeln(S;).
 statement(S)  :- writeln(S;). 
 
 % helper predicates
+% special case is handling the '\=' operator and transforming it into '!=' instead.
 isExpr(X) :- 
 	X =.. [F,A,B], 
-	member(F, [+,-,*,/, >, <, >=, \=, =<, mod, and, or, /\, \/, <<, >>, xor]), !, isExpr(A), isExpr(B), 
+	(member(F, [+,-,*,/, >, <, >=, \=, =<, mod, and, or, /\, \/, <<, >>, xor]), !, isExpr(A), isExpr(B), 
 	write('('), write(A), 
-	write(F), 
-	write(B), write(')').
+	(F == \= -> write('!=') ; write(F)), 
+	write(B), write(')')).
 	
 isExpr(X) :- identifier(X), ! ; value(X).
 
@@ -76,30 +89,7 @@ rest_statements       :- true.
 
 /*
 
-Cast Statement:
-
-statement( 0 :: { b=a; }; 1 :: { b=c; }; ).
-
-
-while (x>0) do {
-  switch (y+z) of {
-0 :: { if (x<10) then { a = 1 ; } else { a = 2; }; }; 1 ::{if(y\=1)then{a=2; b=3;};}; default :: { a = 0 ; } ;
-};
-a=a+1; };
-
-
-while (x>0) {
-   switch (y+z) {
-   case 0 : { if (x<0) { a = 1 ; } else { a = 2 ; } }
-            break ;
-   case 1 : { if (y!=1) { a = 2 ; b = 3 ; } }
-            break ;
-   default: { a = 0 ; }
-			break ; 
-   }
-   a = a+1 ; }
-
-Test Cases for overall:
+Example invocation:
 
 Code = (
 	while (x<1) do { y=2; };
@@ -112,35 +102,5 @@ Code = (
 	a=a+1; };
 ),
 statement( Code ).
-
-Code = (
-	if (x<10) then { a = 1 ; if (x<10) then { a = 1 ; } else { a = 2; };} else { a = 2; };
-),
-statement( Code ).
-
-
-Test Cases for case:
-
-statement( 0 :: { if (x<10) then { a = 1 ; } else { a = 2; }; }; ).
-statement( 0 :: { b=a; }; 1 :: { b=c; }; 2:: { z=b; }; ).
-statement( default :: { if (x<10) then { a = 1 ; } else { a = 2; }; }; ).
-statement( 0 :: { a=b; }; 1 ::{ b=3; }; 2 :: { a=0; }; ).
-
-
-
-Test Cases for if:
-
-statement( if (z\=0) then { x=a; while (a\=0) do { b=a; if (z\=0) then { x=a; } else { b\=a; };}; }; ).
-
-
-Test Cases for while:
-
-statement( while (a\=0) do { b=a; }; ).
-statement( while (a<0) do { b=a; a=c; b=d; }; ).
-statement( while (a<0) do { while (a<0) do { b=a; }; a=c; b=d; }; ).
-statement( while (a<0) do { while (a<0) do { while (a<0) do { b=a; }; }; while (a<0) do { b=a; }; while (a<0) do { b=a; }; }; ).
-statement( a=b; while (a<0) do { while (a<0) do { while (a<0) do { b=a; }; }; while (a<0) do { b=a; }; while (a<0) do { b=a; }; }; ).
-statement( a=b; while (a<0) do { while (a<0) do { b=a; }; a=c; b=d; }; a=b; b=c; while (a<0) do { b=a; }; b=c;).
-
 
 */
