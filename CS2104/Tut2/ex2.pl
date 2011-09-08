e874c59acd61345e2c550ebdf9a6b8f53457b8d1
Benjamin Tan <ben@witsvale.com>
2011-09-08 17:41:16 +0800

    quicksort prolog

--- CS2104/Tut2/ex2.pl
+++ CS2104/Tut2/ex2.pl
@@ -1,34 +1,20 @@
 % Exercise 2
 % Implement Quicksort in Prolog
 
-
 % Base Case
-QuickSort([], [], []).
+quicksort([], []).
+quicksort([HEAD | TAIL], SORTED) :- 
+	partition(HEAD, TAIL, LEFT, RIGHT),
+    quicksort(LEFT, SORTEDL),
+    quicksort(RIGHT, SORTEDR),
+    append(SORTEDL, [HEAD | SORTEDR], SORTED).
 
+/* [+,+,-,-] */
+partition(PIVOT, [], [], []).
+partition(PIVOT, [HEAD | TAIL], [HEAD | LEFT], RIGHT) :- 
+	HEAD =< PIVOT,
+    partition(PIVOT, TAIL, LEFT, RIGHT).
 
-
-void quicksort (int[] a, int lo, int hi)
-{
-//  lo is the lower index, hi is the upper index
-//  of the region of array a that is to be sorted
-    int i=lo, j=hi, h;
-
-    // comparison element x
-    int x=a[(lo+hi)/2];
-
-    //  partition
-    do
-    {    
-        while (a[i]<x) i++; 
-        while (a[j]>x) j--;
-        if (i<=j)
-        {
-            h=a[i]; a[i]=a[j]; a[j]=h;
-            i++; j--;
-        }
-    } while (i<=j);
-
-    //  recursion
-    if (lo<j) quicksort(a, lo, j);
-    if (i<hi) quicksort(a, i, hi);
-}
+partition(PIVOT, [HEAD | TAIL], LEFT, [HEAD | RIGHT]) :- 
+	HEAD > PIVOT,
+    partition(PIVOT, TAIL, LEFT, RIGHT).
