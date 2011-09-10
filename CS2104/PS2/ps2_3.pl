690505c4ac9901975d5c2f034159aa5ee948af8b
Benjamin Tan <ben@witsvale.com>
2011-09-10 16:57:08 +0800

    started on problem set 2, question 1

--- CS2104/PS2/ps2_3.pl
+++ CS2104/PS2/ps2_3.pl
@@ -48,19 +48,51 @@ derive(_, _, 0).
 
 
 % Simplify
-Write a Prolog program that performs arithmetic expression simplification. 
+% Write a Prolog program that performs arithmetic expression simplification. 
 
 % Your program should at least eliminate multiplications by 0 and 1, 
 % and additions with 0. 
-simplify(X+Y, R) :- number(X), number(Y), R is X+Y.
-simplify(X*Y, R) :- number(X), number(Y), R is X*Y.
+simplify(X+Y, R) :-
+	(number(R1), number(R2) -> R is R1+R2, !; 
+	
+	(
+		atom(X), number(Y) -> 
+			simplify(Y, R1), simplify(X, R2); 
+			simplify(X, R1), simplify(Y, R2)
+	), R = R1+R2,!).
+
+simplify(X-Y, R) :-
+	simplify(X, R1),
+	simplify(Y, R2),
+	(number(R1), number(R2) -> R is R1-R2, !; R = R1-R2, !).
+
+
+
+
+/* simplify(X*Y, R) :- (atom(X),number(Y)) -> simplify(Y*X, R), !.*/
+	
+simplify(X*Y, R) :-
+	(X is 0, R = 0), !;
+	(X is 1, R = Y), !;
+	((number(X),number(Y)) -> R is X*Y; R = X*Y, !);
+	((isChar(X),isChar(Y)), R = X*Y; !).
+	
+
+
+
 simplify(X-Y, R) :- number(X), number(Y), R is X-Y.
 simplify(X/Y, R) :- number(X), number(Y), R is X/Y.
+simplify(X, R) :- number(X), R is X, !.
+simplify(X, R) :- atom(X), R = X, !.
+
+
+
 
 % Ideally, it should also convert any subexpression containing only constants 
 % into the value of that expression, and convert multiplications with -1 into 
 % the negative of the multiplicand. 
-
+isChar(Expr) :- atom(Expr) ; number(Expr). 
+isExpr(Expr) :- \+(atom(Expr);number(Expr)).
 
 
 
